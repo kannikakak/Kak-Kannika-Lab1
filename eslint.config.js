ec2df1e7b943a368cb972962e5e1b04089973b9f
kannikakak <kakkannika896@gmail.com>
2025-11-03 14:18:41 +0700

    Initial Node.js project with test and lint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+    },
+    rules: {
+      // Add custom rules here if needed
+    },
+  },
+];
