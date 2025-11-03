c7e5e5bd542377f567e1b0899f88f3edcbc39187
kannikakak <kakkannika896@gmail.com>
2025-11-03 14:50:07 +0700

    Migrate to ES modules

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello, GitHub Actions!');
 });
-module.exports = app;
+export default app;
